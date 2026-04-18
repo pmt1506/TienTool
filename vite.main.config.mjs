f2c2c3e8ffaadaadac4919fbc3521d911de273e0
pmt1506 <pmtien150603@gmail.com>
2026-04-18 14:40:56 +0700

    fix build yml

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -1,23 +1,34 @@
 import { defineConfig } from 'vite';
+import { builtinModules } from 'node:module';
+
+const builtins = [
+  'electron',
+  ...builtinModules.map((m) => [m, `node:${m}`]).flat(),
+];
 
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
+    outDir: '.vite/build',
+    lib: {
+      entry: 'src/main.js',
+      formats: ['cjs'],
+      fileName: () => 'main.js',
+    },
     rollupOptions: {
       external: [
+        ...builtins,
         'mongodb',
-        'kerberos',
-        'os-dns-native',
-        'bson-ext',
-        '@mongodb-js/zstd',
-        '@aws-sdk/credential-providers',
-        'snappy',
-        'socks',
-        'aws4',
-        'mongodb-client-encryption',
-        'gcp-metadata',
         'koffi',
+        'electron-log',
+        'electron-updater',
+        'electron-squirrel-startup',
       ],
     },
+    emptyOutDir: false,
+  },
+  define: {
+    MAIN_WINDOW_VITE_NAME: JSON.stringify('main_window'),
+    MAIN_WINDOW_VITE_DEV_SERVER_URL: 'undefined',
   },
 });
--- vite.preload.config.mjs
+++ vite.preload.config.mjs
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite';
+import { builtinModules } from 'node:module';
+
+const builtins = [
+  'electron',
+  ...builtinModules.map((m) => [m, `node:${m}`]).flat(),
+];
 
 // https://vitejs.dev/config
 export default defineConfig({
   build: {
-    rollupOptions: {
-      external: ['electron'],
+    outDir: '.vite/build',
+    lib: {
+      entry: 'src/preload.js',
+      formats: ['cjs'],
+      fileName: () => 'preload.js',
     },
+    rollupOptions: {
+      external: [
+        ...builtins,
+      ],
+    },
+    emptyOutDir: false,
   },
 });
